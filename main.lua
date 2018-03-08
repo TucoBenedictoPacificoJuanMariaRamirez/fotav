42ffbd49e48c0985981eb7054d90daf13a47c04c
Bence <truewerassad@gmail.com>
2018-03-08 15:00:20 +0100

    First-run project structure
    First version of the project structure

--- main.lua
+++ main.lua
@@ -5,3 +5,7 @@
 -----------------------------------------------------------------------------------------
 
 -- Your code here
+
+-- A main feladata sztem csak annyi lenne, hogy bevezet pár super globális változót
+-- (ha kell ilyen, pl. verzió vagy vmi konstans sztring) és utána átadja a vezérlést
+-- valamelyik controller-nek
