0aeef4062969d541e3f477489f58cd84be3b9347
denzel96 <szabo.daniel1996@gmail.com>
2018-03-29 15:33:51 +0200

    Update logic.lua

--- logic/logic.lua
+++ logic/logic.lua
@@ -6,8 +6,13 @@ require("logic.mapProperties")
 --[[
     TODO:
         - function to call when a pipe is tapped
-        - main thread function for decrementing house temps
-        - main thread timer function for the time limit
+        - timer for decrementing house temps with modifyHouseTemp()
+        - level timer function
+        - endgame rating (0-3 stars)
+        - endgame condition in level timer
+        - implement physical max and min house temp bounds in pipeTap and modifyHouseTemp with respect to connected pipe temps
+            -- min: houseTemp > 10 (v1.1: winter lvl? freezing water?)
+            -- max: houseTemp < max(pipeTemps connected)
 --]]
 
 local logic = {}
@@ -25,18 +30,16 @@ function createLevel(levelNum)
     time = level.time
     pipes = level.pipes
     houses = level.houses
-    --[[local logic = {
-        level=level, time=time, pipes=pipes, houses=houses
-    }--]]
-
     return logic
 end
+
 logic.createLevel = createLevel
 
 --called by the eventListener on tap
 function pipeTap(pipe)
     addTemp = pipe.temp / 10
     for i,v in ipairs(pipe.houses) do
+        --ide kell 
         modifyHouseTemp(v, addTemp)
     end
 end
@@ -44,6 +47,25 @@ logic.pipeTap = pipeTap
 
 local function modifyHouseTemp(house, value)
     houses[house].temp = houses[house].temp + value
+end
+
+--called when no time left (lvl endscreen)
+local function rating()
+
+    -- ***: every houseTemp is optimal
+    if() then
+    end 
+    -- **: at least half of the houseTemps are optimal
+    elseif() then
+    end
+
+    -- *: at least the third of the houseTemps are optimal
+    elseif() then
+    end
+
+    -- 0: 
+    else
+
 end
 
 return logic
